a12f3c9e33fe9fd17344030e55a0f448a1ceb0da
Laura Hernández <lhvarela@gmail.com>
2022-03-08 12:33:01 +0100

    Added interface TitleProps to type the title props in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,31 @@
 import styled from "styled-components";
-import { primary } from "../../styles/globalStyles";
+import { primary, secondary } from "../../styles/globalStyles";
 
 const TitlePage = styled.h1`
   font-size: 22px;
   color: ${primary};
 `;
 
-const Header = (): JSX.Element => {
+const ContainerHeader = styled.div`
+  background-color: ${secondary};
+`;
+
+interface TitleProps {
+  title: string;
+}
+
+const Header = ({ title }: TitleProps): JSX.Element => {
   return (
     <>
-      <TitlePage>title</TitlePage>
-      <img width={"22px"} src="images/favicon.png" alt="Skybuzz logo" />
-      <img
-        width={"26px"}
-        src="./images/burger-menu.png"
-        alt="menu with 'about us' and logout options"
-      />
+      <ContainerHeader className="container">
+        <TitlePage>{title}</TitlePage>
+        <img width="40px" src="images/favicon.png" alt="Skybuzz logo" />
+        <img
+          width="30px"
+          src="images/burger-menu.png"
+          alt="menu with 'about us' and logout options"
+        />
+      </ContainerHeader>
     </>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <>
       <h1>Homepage</h1>
-      <Header />
+      <Header title="Buzzs" />
     </>
   );
 };
