9bab4e2b6b51e2edf0960ec8eec9d642ad241f11
Laura Hernández <lhvarela@gmail.com>
2022-03-08 11:35:38 +0100

    Changed extensions in sonar configs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import Header from "../components/Header/Header";
+
 const HomePage = () => {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <h1>Homepage</h1>
+      <Header />
+    </>
+  );
 };
 
 export default HomePage;
