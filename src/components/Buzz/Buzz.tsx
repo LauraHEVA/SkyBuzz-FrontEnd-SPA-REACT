1943892185b0b3e73790088acee6fb190d2cce1c
Laura Hernández <lhvarela@gmail.com>
2022-03-09 10:35:45 +0100

    Interface to buzz object created

--- src/components/Buzz/Buzz.tsx
+++ src/components/Buzz/Buzz.tsx
@@ -1,7 +1,20 @@
-const Buzz = (): JSX.Element => {
+interface BuzzProps {
+  buzz: BuzzObject;
+}
+interface BuzzObject {
+  category: string;
+  likes: number;
+  comments: number[];
+  author: string;
+  text: string;
+  date: string;
+  id: string;
+}
+
+const Buzz = ({ buzz }: BuzzProps): JSX.Element => {
   return (
     <>
-      <p>Buzz</p>
+      <p>{buzz.author}</p>
       <p>Buzz</p>
     </>
   );
