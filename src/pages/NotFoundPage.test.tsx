cfe3cefa354d1af511264ae2278d707534294be2
Laura Hernández <lhvarela@gmail.com>
2022-03-10 22:58:00 +0100

    Added new test to notFoundPage element

---
+++ src/pages/NotFoundPage.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../redux/store";
+import NotFoundPage from "./NotFoundPage";
+
+describe("Given a NotFoundPage component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should render a h2 element with the text 'Ups...There is nothing in this page'", () => {
+      const expectedText = "Ups...There is nothing in this page";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <NotFoundPage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const foundHeading = screen.getByRole("heading", { level: 2 });
+
+      expect(foundHeading).toBe(expectedText);
+    });
+  });
+});
