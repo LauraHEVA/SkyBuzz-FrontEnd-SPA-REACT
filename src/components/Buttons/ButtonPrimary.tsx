437f7db9ee99a31548bf19cc2758301056024b56
Laura Hernández <lhvarela@gmail.com>
2022-03-10 12:05:57 +0100

    Added styles in primary button

--- src/components/Buttons/ButtonPrimary.tsx
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
-import { primary } from "../../styles/globalStyles";
+import { greenCircleAuthor, grey, primary } from "../../styles/globalStyles";
 import { ButtonProps } from "../../types/componentsInterfaces";
 
 const PrimaryBtn = styled.button`
   background-color: ${primary};
+  font-family: poppins;
+  font-size: 16px;
+  font-weight: 900;
+  letter-spacing: 1px;
+  border-radius: 10px;
+  width: 165px;
+  height: 45px;
+  color: ${greenCircleAuthor};
+  border: 1px solid ${greenCircleAuthor};
+  box-shadow: 0 4px 10px ${grey};
 `;
 
 const ButtonPrimary = ({
