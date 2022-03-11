abc6bdce4137f3c8851728cc376fbe630c074d91
Laura Hernández <lhvarela@gmail.com>
2022-03-11 11:11:32 +0100

    Added new test to reducer with delete action

--- src/redux/reducers/buzzsReducer.test.ts
+++ src/redux/reducers/buzzsReducer.test.ts
@@ -62,4 +62,50 @@ describe("Given a buzzsReducer function", () => {
       expect(newState).toEqual(currentBuzzs);
     });
   });
+
+  describe("When it's called with a deleteBuzz action and an id", () => {
+    test("Then it should return a new state without the buzz deleted", () => {
+      const currentBuzzs: BuzzObject[] = [
+        {
+          category: "general",
+          likes: 0,
+          comments: [1],
+          author: "Madonna",
+          text: "Madonna is writting a message Madonna is writting a very long and repeated message Madonna is writting a message Madonna is writting a message Madonna is writting a message",
+          date: "2022-03-08T19:21:30.764Z",
+          id: "324k2l",
+        },
+        {
+          category: "general",
+          likes: 0,
+          comments: [1],
+          author: "Freddie",
+          text: "Freddie is writting a very long and repeated message",
+          date: "2022-03-08T19:22:30.764Z",
+          id: "32454544",
+        },
+      ];
+
+      const action = {
+        type: "delete-one-buzz",
+        id: "32454544",
+      };
+
+      const newBuzzsWithoutIdDeleted = [
+        {
+          category: "general",
+          likes: 0,
+          comments: [1],
+          author: "Madonna",
+          text: "Madonna is writting a message Madonna is writting a very long and repeated message Madonna is writting a message Madonna is writting a message Madonna is writting a message",
+          date: "2022-03-08T19:21:30.764Z",
+          id: "324k2l",
+        },
+      ];
+
+      const newState = buzzsReducer(currentBuzzs, action);
+
+      expect(newState).toEqual(newBuzzsWithoutIdDeleted);
+    });
+  });
 });
