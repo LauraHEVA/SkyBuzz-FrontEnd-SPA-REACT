297569166668d999e35c38120c5defdd19f3c8f6
Laura Hernández <lhvarela@gmail.com>
2022-03-11 12:19:54 +0100

    Changed styled components in buzz component to styles folder

--- src/components/Buzz/Buzz.tsx
+++ src/components/Buzz/Buzz.tsx
@@ -1,75 +1,23 @@
-import styled from "styled-components";
 import { BuzzProps } from "../../types/buzzInterfaces";
 import {
   ListItem,
   ListUlHorizontal,
 } from "../../styles/globalStyledComponents";
-import {
-  grey,
-  greenCircleAuthor,
-  greyLight,
-  primary,
-} from "../../styles/globalStyles";
+
 import CommentIcon from "../CommentIcon/CommentIcon";
 import HeartIcon from "../HeartIcon/HeartIcon";
 import TrashIcon from "../TrashIcon/TrashIcon";
-
-const MainContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-  border-bottom: 1px solid ${greyLight};
-  margin-bottom: 20px;
-  width: 100%;
-`;
-
-const ProfileContainer = styled.div`
-  padding-right: 5px;
-`;
-
-const ProfileCircle = styled.div`
-  font-family: "Permanent Marker";
-  height: 50px;
-  width: 50px;
-  border-radius: 50%;
-  background-color: ${greenCircleAuthor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const MessageContainer = styled.div`
-  padding-left: 8px;
-  width: 100%;
-`;
-
-const TextAuthor = styled.p`
-  color: ${primary};
-  font-weight: 900;
-  font-size: 14px;
-  text-shadow: 0 3px 3px ${grey};
-  margin-top: 0;
-`;
-
-const TextBuzz = styled.p`
-  color: ${primary};
-  font-size: 14px;
-`;
-
-const TextCategory = styled.p`
-  color: ${primary};
-  font-weight: 900;
-  font-size: 12px;
-`;
-
-const TextTimeAgo = styled.span`
-  color: ${primary};
-  font-size: 14px;
-`;
-
-const ContainerSuperior = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
+import {
+  ContainerSuperior,
+  MainContainer,
+  MessageContainer,
+  ProfileCircle,
+  ProfileContainer,
+  TextAuthor,
+  TextBuzz,
+  TextCategory,
+  TextTimeAgo,
+} from "../../styles/buzzComponentStyles";
 
 const Buzz = ({ buzz, onClick }: BuzzProps): JSX.Element => {
   return (
@@ -88,7 +36,6 @@ const Buzz = ({ buzz, onClick }: BuzzProps): JSX.Element => {
           </ContainerSuperior>
           <TextBuzz>{buzz.text}</TextBuzz>
           <TextCategory>#{buzz.category}</TextCategory>
-
           <ListUlHorizontal>
             <ListItem>
               <TextTimeAgo>1h</TextTimeAgo>
