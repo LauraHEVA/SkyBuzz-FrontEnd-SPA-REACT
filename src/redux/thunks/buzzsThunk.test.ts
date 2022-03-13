f04f2e489f305b07622f4f4dd7fb28c5c54b0bb0
Laura Hernández <lhvarela@gmail.com>
2022-03-13 11:44:03 +0100

    Added test thunk to deleteBuzzThunk

--- src/redux/thunks/buzzsThunk.test.ts
+++ src/redux/thunks/buzzsThunk.test.ts
@@ -1,4 +1,4 @@
-import { loadAllBuzzsThunk } from "./buzzsThunk";
+import { deleteBuzzThunk, loadAllBuzzsThunk } from "./buzzsThunk";
 
 describe("Given a loadAllBuzzsThunk function", () => {
   describe("When it is called", () => {
@@ -6,6 +6,27 @@ describe("Given a loadAllBuzzsThunk function", () => {
       const dispatch = jest.fn();
       await loadAllBuzzsThunk(dispatch);
       expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a deleteBuzzThunk", () => {
+  describe("When it's called with an id", () => {
+    test("Then it should call dispatch with de deleteBuzzAction as parameter", async () => {
+      const id = "324k2lB";
+
+      const expectedAction = {
+        type: "delete-one-buzz",
+        id: id,
+      };
+
+      const dispatch = jest.fn();
+
+      const deleteThunk = deleteBuzzThunk(id);
+
+      await deleteThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
     });
   });
 });
