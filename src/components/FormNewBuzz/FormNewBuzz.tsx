76fec801e7dc382b7743d6e1188b2759e532993f
Laura Hernández <lhvarela@gmail.com>
2022-03-13 21:17:42 +0100

    Form to create a new buzz with semantic html and onchange events

--- src/components/FormNewBuzz/FormNewBuzz.tsx
+++ src/components/FormNewBuzz/FormNewBuzz.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import {
@@ -11,44 +11,52 @@ import Toastr from "../Toastr/Toastr";
 const FormNewBuzz = (): JSX.Element => {
   const navigate = useNavigate();
 
-  const goToHomePage = () => {
-    navigate(`/home`);
+  const [topicValue, setTopicValue] = useState<string>("");
+  const [messageValue, setMessageValue] = useState<string>("");
+
+  const resetForm = () => {
+    setTopicValue("");
+    setMessageValue("");
   };
 
-  const emptyDataForm = {
-    topicBuzz: "",
-    messageBuzz: "",
-  };
-
-  const [formData, setFormData] = useState(emptyDataForm);
-
-  const formSubmit = (event: React.SyntheticEvent) => {
+  const onSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
-    // dispatch(registerUserThunk(formData));
-    // resetForm();
-    goToHomePage();
+    resetForm();
+    navigate(`/home`);
   };
 
   return (
     <ContainerFormNewBuzz className="container">
-      <form onSubmit={formSubmit} autoComplete="off">
+      <form onSubmit={onSubmit} autoComplete="off">
         <div className="input-group mb-3">
           <label className="input-group-text" htmlFor="topicBuzz">
             Topic
           </label>
-          <select className="form-select" id="topicBuzz">
-            <option selected>General</option>
-            <option value="1">Resources</option>
-            <option value="2">PlayTime</option>
-            <option value="3">Events</option>
+          <select
+            className="form-select"
+            id="topicBuzz"
+            value={topicValue}
+            onChange={(ev: React.ChangeEvent<HTMLSelectElement>): void =>
+              setTopicValue(ev.target.value)
+            }
+          >
+            <option value="1">General</option>
+            <option value="2">Resources</option>
+            <option value="3">PlayTime</option>
+            <option value="4">Events</option>
           </select>
         </div>
         <div className="form-floating">
-          <textarea
+          <input
             className="form-control"
             placeholder="Leave a comment here"
             id="messageBuzz"
-          ></textarea>
+            type="text"
+            value={messageValue}
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) =>
+              setMessageValue(ev.target.value)
+            }
+          ></input>
           <label htmlFor="messageBuzz">Writte your buzz here...</label>
         </div>
 
