a1723b63e533a9cf35fe7daf042b51acd7307191
Laura Hernández <lhvarela@gmail.com>
2022-03-13 12:13:57 +0100

    Deleted unnecessary import in trashIcon test

--- src/components/TrashIcon/TrashIcon.test.tsx
+++ src/components/TrashIcon/TrashIcon.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import TrashIcon from "./TrashIcon";
 
 describe("Given TrashIcon component", () => {
