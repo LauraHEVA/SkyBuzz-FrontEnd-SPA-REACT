c679fe042568ea0d39146259985f7a30e5aa4a5a
Laura Hernández <lhvarela@gmail.com>
2022-03-13 11:48:24 +0100

    Added test delete thunk to check inexistent ID

--- src/redux/thunks/buzzsThunk.test.ts
+++ src/redux/thunks/buzzsThunk.test.ts
@@ -11,22 +11,31 @@ describe("Given a loadAllBuzzsThunk function", () => {
 });
 
 describe("Given a deleteBuzzThunk", () => {
-  describe("When it's called with an id", () => {
+  describe("When it's called with an existent id", () => {
     test("Then it should call dispatch with de deleteBuzzAction as parameter", async () => {
       const id = "324k2lB";
-
       const expectedAction = {
         type: "delete-one-buzz",
         id: id,
       };
-
       const dispatch = jest.fn();
 
       const deleteThunk = deleteBuzzThunk(id);
-
       await deleteThunk(dispatch);
 
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+
+  describe("When it's called with an unexistent id", () => {
+    test("Then it shouldn't call dispatch", async () => {
+      const id = "123456";
+      const dispatch = jest.fn();
+
+      const deleteThunk = deleteBuzzThunk(id);
+      await deleteThunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
     });
   });
 });
