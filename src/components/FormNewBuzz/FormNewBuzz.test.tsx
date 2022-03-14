e5d912ae573b44d488b0ba4d156002ba806d0348
Laura Hernández <lhvarela@gmail.com>
2022-03-14 14:48:33 +0100

    Added test to form to ckeck the button

---
+++ src/components/FormNewBuzz/FormNewBuzz.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../redux/store";
+import FormNewBuzz from "./FormNewBuzz";
+
+describe("Given a form component", () => {
+  describe("When it's rendered", () => {
+    test("It should display a button with the text 'Buzz It!", () => {
+      const textButton = "Buzz It!";
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <FormNewBuzz />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const textFounded = screen.getByText(textButton);
+
+      expect(textFounded).toBeInTheDocument();
+    });
+  });
+});
