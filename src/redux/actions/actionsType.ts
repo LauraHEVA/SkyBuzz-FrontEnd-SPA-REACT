13b7f4a21214f09dafe388e743dfbd22e690917f
Laura Hernández <lhvarela@gmail.com>
2022-03-15 15:09:40 +0100

    Added new action type to increment likes

--- src/redux/actions/actionsType.ts
+++ src/redux/actions/actionsType.ts
@@ -2,6 +2,7 @@ const actionsType = {
   loadAllBuzzs: "load-all-buzzs",
   deleteBuzz: "delete-one-buzz",
   addBuzz: "add-new-buzz",
+  incrementLikes: "increment-likes",
 };
 
 export default actionsType;
