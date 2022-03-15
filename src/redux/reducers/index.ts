b778e80f4c8f17b95c6eb2cce579dc6c3002458f
Laura Hernández <lhvarela@gmail.com>
2022-03-15 15:49:23 +0100

    Added buzz state in rootReducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import buzzsReducer from "./buzzsReducer";
+import oneBuzzReducer from "./oneBuzzReducer";
 
 const rootReducer = combineReducers({
   buzzs: buzzsReducer,
+  buzz: oneBuzzReducer,
 });
 
 export default rootReducer;
