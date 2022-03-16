3c063927f3944a408aa52def0af673ccc8b17480
Laura Hernández <lhvarela@gmail.com>
2022-03-16 13:24:12 +0100

    Added content in DetailBuzzPage including the buzz component

--- src/pages/DetailBuzzPage.tsx
+++ src/pages/DetailBuzzPage.tsx
@@ -11,12 +11,40 @@ import { useNavigate } from "react-router-dom";
 import ButtonPrimary from "../components/Buttons/ButtonPrimary";
 import Toastr from "../components/Toastr/Toastr";
 import Footer from "../components/Footer/Footer";
+import { BuzzObject } from "../types/buzzInterfaces";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { useEffect } from "react";
+import {
+  deleteBuzzThunk,
+  incrementLikesThunk,
+} from "../redux/thunks/buzzsThunk";
+import Buzz from "../components/Buzz/Buzz";
+import { toast } from "react-toastify";
 
 const DetailBuzzPage = () => {
   const navigate = useNavigate();
 
   const goToHomePage = () => {
     navigate(`/home`);
+  };
+
+  const dispatch = useDispatch();
+
+  const buzzDetail: BuzzObject = useSelector(
+    (state: RootState) => state.oneBuzz
+  );
+
+  useEffect(() => {
+    dispatch(incrementLikesThunk);
+  }, [dispatch]);
+
+  const deleteBuzz = (id: string) => {
+    dispatch(deleteBuzzThunk(id));
+  };
+
+  const addLikeBuzz = (id: string) => {
+    dispatch(incrementLikesThunk(id));
   };
 
   return (
@@ -27,6 +55,16 @@ const DetailBuzzPage = () => {
       <Toastr />
       <MainContainerPage>
         <p>Detail message here</p>
+        <Buzz
+          buzz={buzzDetail}
+          onClickTrash={() => {
+            deleteBuzz(buzzDetail.id);
+            toast.success("Buzz deleted correctly");
+          }}
+          onClickHeart={() => {
+            addLikeBuzz(buzzDetail.id);
+          }}
+        />
         <ButtonContainerNewBuzz>
           <ButtonPrimary
             actionOnClick={() => {
