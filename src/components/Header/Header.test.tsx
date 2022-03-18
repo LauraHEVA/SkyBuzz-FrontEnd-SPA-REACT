01049cc3fe65088635f5cde11d6077f1a809af78
Laura Hernández <lhvarela@gmail.com>
2022-03-18 19:35:30 +0100

    Fixed text in header component to get the heading

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,16 +1,21 @@
 import Header from "./Header";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given a Header Component", () => {
   describe("When it receives the text 'World'", () => {
     test("Then it should render a title with the text 'World'", () => {
       const text = "World";
 
-      render(<Header title={text} />);
+      render(
+        <BrowserRouter>
+          <Header title={text} />
+        </BrowserRouter>
+      );
 
-      const foundTitle = screen.getByText(text);
+      const foundTitle = screen.getByRole("heading");
 
-      expect(foundTitle).toBeInTheDocument();
+      expect(foundTitle.textContent).toBe(text);
     });
   });
 });
