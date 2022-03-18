7e5a0966abc216725d7b59cf78cdea3885326c5a
Laura Hernández <lhvarela@gmail.com>
2022-03-18 12:05:53 +0100

    Created component LoginPage

---
+++ src/pages/LoginPage .tsx
@@ -0,0 +1,14 @@
+import { MainContainerPage } from "../styles/globalStyledComponents";
+import FormNewBuzz from "../components/FormNewBuzz/FormNewBuzz";
+
+const LoginPage = () => {
+  return (
+    <>
+      <MainContainerPage>
+        <FormNewBuzz />
+      </MainContainerPage>
+    </>
+  );
+};
+
+export default LoginPage;
