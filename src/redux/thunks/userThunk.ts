d6b5dcde0f3379ca2ec9e26fbbb23f8cfa9c8abe
Laura Hernández <lhvarela@gmail.com>
2022-03-18 10:53:15 +0100

    Added interface to Decodedtoken

---
+++ src/redux/thunks/userThunk.ts
@@ -0,0 +1,26 @@
+import jwtDecode from "jwt-decode";
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { LoginData } from "../../types/userInterfaces";
+
+export const loginUserThunk =
+  (user: LoginData) =>
+  async (dispatch: ThunkDispatch<void, unknown, AnyAction>) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/users/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      }
+    );
+
+    if (response.ok) {
+      const token = await response.json();
+      const { id, username } = await jwtDecode(token.token);
+      localStorage.setItem("UserToken", token.token);
+      dispatch(loginUserAction({ id, username, token: token.token }));
+    }
+  };
