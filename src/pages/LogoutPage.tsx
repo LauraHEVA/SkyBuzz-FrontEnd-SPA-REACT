ea6dfa9198c4cb1b7dd499fd814b5a70cd64277b
Laura Hernández <lhvarela@gmail.com>
2022-03-19 17:53:42 +0100

    Added conditional rendering in LogoutPage when user islogged

--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -4,11 +4,16 @@ import {
 } from "../styles/globalStyledComponents";
 import Title from "../components/Title/Title";
 import ButtonPrimary from "../components/Buttons/ButtonPrimary";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUserAction } from "../redux/actions/actionsCreator";
+import { UserData } from "../types/userInterfaces";
+import { RootState } from "../redux/store";
+import LoginPage from "./LoginPage";
 
 const LogoutPage = () => {
+  const user: UserData = useSelector((state: RootState) => state.user);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -24,16 +29,20 @@ const LogoutPage = () => {
 
   return (
     <>
-      <MainContainerPage>
-        <Title title="Are you sure you want to log out?" />
-        <ButtonLogoutContainer>
-          <ButtonPrimary
-            text="Yes, Log out"
-            className="btn-primary"
-            actionOnClick={logOut}
-          ></ButtonPrimary>
-        </ButtonLogoutContainer>
-      </MainContainerPage>
+      {user.loggedIn ? (
+        <MainContainerPage>
+          <Title title="Are you sure you want to log out?" />
+          <ButtonLogoutContainer>
+            <ButtonPrimary
+              text="Yes, Log out"
+              className="btn-primary"
+              actionOnClick={logOut}
+            ></ButtonPrimary>
+          </ButtonLogoutContainer>
+        </MainContainerPage>
+      ) : (
+        <LoginPage />
+      )}
     </>
   );
 };
