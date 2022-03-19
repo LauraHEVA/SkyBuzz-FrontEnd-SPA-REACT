8fab5d2851e68151eab2864aa7e900e84cd70b86
Laura Hernández <lhvarela@gmail.com>
2022-03-19 09:17:28 +0100

    Added styled component in h2 title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,12 +1,20 @@
+import styled from "styled-components";
+import { primary } from "../../styles/globalStyles";
 import { TitleProps } from "../../types/componentsInterfaces";
 
 const Title = ({ title }: TitleProps) => {
   return (
     <>
-      <h2>SkyBuzz</h2>
+      <H2Login>SkyBuzz</H2Login>
       <h3>{title}</h3>
     </>
   );
 };
+
+const H2Login = styled.h2`
+  color: ${primary};
+  font-size: 64px;
+  font-family: Mukta, nunito, sans-serif;
+`;
 
 export default Title;
