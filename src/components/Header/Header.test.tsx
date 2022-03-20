83a78c80d60e64d04a29ff5e22de9a6b719ed8ca
Laura Hernández <lhvarela@gmail.com>
2022-03-20 18:02:37 +0100

    Added test in header component to check the links with the CR

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
 
 describe("Given a Header Component", () => {
   describe("When it receives the text 'World'", () => {
@@ -20,6 +22,50 @@ describe("Given a Header Component", () => {
       const foundTitle = screen.getByRole("heading");
 
       expect(foundTitle.textContent).toBe(text);
+    });
+  });
+
+  describe("When the user is logged in", () => {
+    test("Then it should render a link with the text 'Log Out'", () => {
+      const mockStore = configureMockStore([thunk]);
+      const storeMock = mockStore({
+        user: { loggedIn: true },
+      });
+      const text = "Log Out";
+
+      render(
+        <Provider store={storeMock}>
+          <BrowserRouter>
+            <Header title={text} />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const foundLink = screen.getByRole("link", { name: text });
+
+      expect(foundLink).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user is logged out", () => {
+    test("Then it should render a link with the text 'Log In'", () => {
+      const mockStore = configureMockStore([thunk]);
+      const storeMock = mockStore({
+        user: { loggedIn: false },
+      });
+      const text = "Log In";
+
+      render(
+        <Provider store={storeMock}>
+          <BrowserRouter>
+            <Header title={text} />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const foundLink = screen.getByRole("link", { name: text });
+
+      expect(foundLink).toBeInTheDocument();
     });
   });
 });
