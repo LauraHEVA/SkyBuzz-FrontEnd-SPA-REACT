52431dc44b1ecd268d47e3c4ea8adb6d3dfd4dfb
Laura Hernández <lhvarela@gmail.com>
2022-03-21 20:39:11 +0100

    Added service worker file

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.NetworkFirst()
+);
