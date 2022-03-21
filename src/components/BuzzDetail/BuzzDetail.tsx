90f561507de03cbd62bf02d0095ac2cc5fa28ad4
Laura Hernández <lhvarela@gmail.com>
2022-03-21 12:07:50 +0100

    Created BuzzDetail component without cursor pointer in text

---
+++ src/components/BuzzDetail/BuzzDetail.tsx
@@ -0,0 +1,67 @@
+import { BuzzProps } from "../../types/buzzInterfaces";
+import {
+  ListItem,
+  ListUlHorizontal,
+} from "../../styles/globalStyledComponents";
+
+import CommentIcon from "../CommentIcon/CommentIcon";
+import HeartIcon from "../HeartIcon/HeartIcon";
+import TrashIcon from "../TrashIcon/TrashIcon";
+import {
+  ContainerSuperior,
+  MessageContainer,
+  ProfileCircle,
+  ProfileContainer,
+  TextAuthor,
+  TextBuzz,
+  TextTopic,
+  TextTimeAgo,
+  NumDataIcons,
+  BuzzCardContainer,
+  TextBuzzDetail,
+} from "../../styles/buzzComponentStyles";
+import ReactTimeAgo from "react-time-ago";
+import { useNavigate } from "react-router-dom";
+
+const BuzzDetail = ({
+  buzz,
+  onClickTrash,
+  onClickHeart,
+}: BuzzProps): JSX.Element => {
+  return (
+    <>
+      <BuzzCardContainer>
+        <ProfileContainer>
+          <ProfileCircle>
+            <span>{buzz.author.name.substring(0, 2).toUpperCase()}</span>
+          </ProfileCircle>
+        </ProfileContainer>
+        <MessageContainer>
+          <ContainerSuperior>
+            <TextAuthor>{buzz.author.name}</TextAuthor>
+            <TextTimeAgo>
+              <ReactTimeAgo date={Date.parse(buzz.date)} locale="en-US" />
+            </TextTimeAgo>
+          </ContainerSuperior>
+          <TextBuzzDetail>{buzz.text}</TextBuzzDetail>
+          <TextTopic>#{buzz.topic}</TextTopic>
+          <ListUlHorizontal>
+            <ListItem>
+              <NumDataIcons>{buzz.comments.length}</NumDataIcons>
+              <CommentIcon />
+            </ListItem>
+            <ListItem>
+              <NumDataIcons>{buzz.likes}</NumDataIcons>
+              <HeartIcon onClick={onClickHeart} />
+            </ListItem>
+            <ListItem>
+              <TrashIcon onClick={onClickTrash} />
+            </ListItem>
+          </ListUlHorizontal>
+        </MessageContainer>
+      </BuzzCardContainer>
+    </>
+  );
+};
+
+export default BuzzDetail;
