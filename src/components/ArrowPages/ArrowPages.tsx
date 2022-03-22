84a5ebdb0a23d0e23bf071d11492daf043df5e18
Laura Hernández <lhvarela@gmail.com>
2022-03-22 20:55:36 +0100

    Changed colors in arrow pages component

--- src/components/ArrowPages/ArrowPages.tsx
+++ src/components/ArrowPages/ArrowPages.tsx
@@ -1,7 +1,7 @@
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { background, primary } from "../../styles/globalStyles";
+import { background, greyLight, primary } from "../../styles/globalStyles";
 import { ArrowPagesProps } from "../../types/componentsInterfaces";
 
 const ArrowPages = ({
@@ -15,7 +15,7 @@ const ArrowPages = ({
         data-testid="pageArrow"
         icon={showSide ? faAngleLeft : faAngleRight}
         size="2x"
-        color={disabled ? "dimgray" : primary}
+        color={disabled ? greyLight : primary}
       />
     </ArrowPagesBtn>
   );
