12894e6f1d9291b0a2103a669b87203643786b2d
Laura Hernández <lhvarela@gmail.com>
2022-03-23 15:38:00 +0100

    Tested the arrow pages in home page when the list of buzzs is >10

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -93,4 +93,180 @@ describe("Given a HomePage component", () => {
       expect(buttonFound).toBeInTheDocument();
     });
   });
+
+  describe("When it's rendered with an array of 11 buzzs", () => {
+    test("Then it should render the arrow icons to change pages", async () => {
+      const mockStore = configureMockStore([thunk]);
+      const storeMock = mockStore({
+        user: { loggedIn: true },
+        buzzs: [
+          {
+            topic: "general",
+            likes: 0,
+            comments: [],
+            author: {
+              name: "Dan",
+              username: "Danilovic",
+              id: "623245decaa7d69f96f10a95",
+            },
+            text: "React Redux",
+            id: "324k2lA",
+            date: "2022-03-12T14:14:10.573Z",
+          },
+          {
+            topic: "general",
+            likes: 0,
+            comments: [],
+            author: {
+              name: "Dan",
+              username: "Abramov",
+              id: "623245decaa7d69f96f10a95",
+            },
+            text: "I Hate Typescript",
+            id: "324k2lB",
+            date: "2022-03-12T14:14:10.573Z",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "623ae7ce2c2779aaa8df6b38",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324k2lBcad",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324k2lasfwd",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324kabcde",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324k2l1234",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "32412445d",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324kabcdddde",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "324k2laaaaa34",
+          },
+          {
+            author: {
+              name: "Guillem",
+              username: "Guillem0",
+              id: "62361397ae106fd04add2d52",
+            },
+            text: "Yo sinceramente no lo tengo en apuntes y ya esta perdidisimo en el mar del olvido",
+            topic: "resources",
+            date: "2022-03-23T09:26:38.590Z",
+            likes: 0,
+            isComment: false,
+            comments: [],
+            id: "3241244fffff5d",
+          },
+        ],
+      });
+      const dataTest = "pageArrow";
+
+      render(
+        <BrowserRouter>
+          <Provider store={storeMock}>
+            <HomePage />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const foundElement = screen.queryAllByTestId(dataTest);
+
+      expect(foundElement).toHaveLength(2);
+    });
+  });
 });
