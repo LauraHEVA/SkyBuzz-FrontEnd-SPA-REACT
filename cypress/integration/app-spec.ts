66b3346b57853455ec3c0cb49b6d67a598c8e02b
Laura Hernández <lhvarela@gmail.com>
2022-03-23 20:14:19 +0100

    Fixed test cypress with correct path to get logged in

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -7,15 +7,13 @@ describe("App", function () {
   });
 
   it("renders the app should find h2 with login", function () {
-    cy.get("h2").should("contain", "Log in");
+    cy.get("h3").should("contain", "Sign in and start buzzing");
   });
 
-  it("should find the 1st input and type kevin", function () {
-    cy.get("input").first().type("kevin").should("have.value", "Luis0");
-  });
-
-  it("should find the 2md input and type kevin", function () {
-    cy.get("input").last().type("kevin").should("have.value", "Luis1234");
+  it("should find the 1st input and type Luis0, then should find the 2nd input and type Luis1234", function () {
+    cy.get("input").first().type("Luis0").should("have.value", "Luis0");
+    cy.get("input").last().type("Luis1234").should("have.value", "Luis1234");
+    cy.get("button").contains("Log in").click();
   });
 });
 
