129b596cb150ed6906c88708918c9caaa0a7ee68
Laura Hernández <lhvarela@gmail.com>
2022-03-23 15:08:22 +0100

    New test in BuzzDetail component

--- src/components/BuzzDetail/BuzzDetail.test.tsx
+++ src/components/BuzzDetail/BuzzDetail.test.tsx
@@ -48,4 +48,53 @@ describe("Given BuzzDetail Component", () => {
       expect(foundText).toBeInTheDocument();
     });
   });
+
+  describe("When its rendered with a buzz that has a comment with the text 'Pulp Fiction' in the array of comments", () => {
+    test("Then it should display a paragraph with the text 'Pulp Fiction'", () => {
+      const textMessage = "Pulp Fiction";
+      const buzzFake = {
+        topic: "general",
+        likes: 0,
+        comments: [
+          {
+            author: {
+              name: "Laura",
+              username: "Laura0",
+              id: "623245decaa7d69f96f10a95",
+            },
+            text: "Pulp Fiction",
+            topic: "general",
+            likes: 0,
+            date: "2022-03-21T23:40:07.555Z",
+            id: "62390cd7815f7c4c0b411f6f",
+          },
+        ],
+        author: {
+          name: "LauHater",
+          id: "623245decaa7d69f96f10a95",
+          username: "Laura0",
+        },
+        text: "Typescript I Hate U",
+        id: "324k2l",
+        date: new Date().toDateString(),
+      };
+      TimeAgo.addLocale(en);
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <BuzzDetail
+              buzz={buzzFake}
+              onClickTrash={() => {}}
+              onClickHeart={() => {}}
+            />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const foundText = screen.getByText(textMessage);
+
+      expect(foundText).toBeInTheDocument();
+    });
+  });
 });
