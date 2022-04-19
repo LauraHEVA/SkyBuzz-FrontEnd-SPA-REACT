416703a53cb02a7e7aec57ea2f3ccfb5d1eee054
Laura Hernández <lhvarela@gmail.com>
2022-04-19 20:55:11 +0200

    Revert "Changed main colours to super awesome purple pallette"
    This reverts commit e8dde60452d43d6b50fe97cbcb26b2e20d113ff2.

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,16 +1,9 @@
-// export const primary = "#086375"; // Blue Lagoon
-// export const secondary = "#a8dadc"; // Charlotte
-// export const background = "#DEF7EC"; // Green-light background
-
-export const primary = "#560bad"; // Blue Lagoon
-export const secondary = "#cfbaf0"; // Charlotte
-export const background = "#f8edeb"; // Green-light background
+export const primary = "#086375"; // Blue Lagoon
+export const secondary = "#a8dadc"; // Charlotte
+export const background = "#DEF7EC"; // Green-light background
 export const backgroundSecondary = "#ffffff";
 export const grey = "#8D99AE"; // Grey light
-
-// export const greyLight = "#87bba2"; // Grey light border bottom
-export const greyLight = "#cdb4db"; // Grey light border bottom
-
+export const greyLight = "#87bba2"; // Grey light border bottom
 export const greyCyan = "#68EAC6"; // Grey Cyan
 
 export const breakpointXS = "600px"; // Small screens
